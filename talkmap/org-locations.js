24d2fd32f366f61444ca17354718c8bfda468e2e
Dimitrios - Georgios Kontopoulos <dgkontopoulos@gmail.com>
2023-10-01 19:46:22 +0200

    Updated the CV.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -30,23 +30,13 @@ var addressPoints = [
     26.554788
   ], 
   [
-    "\u0391\u03b8\u03ae\u03bd\u03b1, \u0395\u03bb\u03bb\u03ac\u03b4\u03b1", 
-    37.9755648, 
-    23.7348324
-  ], 
-  [
     "Portland, OR, United States of America", 
     45.5202471, 
     -122.674194
   ], 
   [
     "London, United Kingdom", 
-    51.5073359, 
-    -0.12765
-  ], 
-  [
-    "Ko\u0161ice, Slovakia", 
-    48.7172272, 
-    21.2496774
+    51.4893335, 
+    -0.14405508452768728
   ]
 ];
